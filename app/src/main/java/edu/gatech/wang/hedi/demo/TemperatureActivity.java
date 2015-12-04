68df9d5a1d74a2b543ce581a3a2101445b866710
Hedi Wang <wanghedi1991@gmail.com>
2015-12-04 16:09:21 -0500

    aaaaa

---
+++ app/src/main/java/edu/gatech/wang/hedi/demo/TemperatureActivity.java
@@ -0,0 +1,48 @@
+package edu.gatech.wang.hedi.demo;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class TemperatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_temperature, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add_item) {
+            LinearLayout itemList = (LinearLayout) findViewById(R.id.temperature_list);
+            View view = new View(this);
+            view.inflate(TemperatureActivity.this, R.layout.temperature_item_layout, itemList);
+            itemList.addView(view);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
